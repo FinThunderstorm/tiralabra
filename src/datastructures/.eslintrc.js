c3a59741c5b504ff8fd0be9ba99bfe8f8cfeaaa5
Tuomas Alanen <tualanen@cs.helsinki.fi>
2021-11-06 23:07:13 +0200

    konfiguraatiotiedostot vastaamaan annettuja airbnb:n tyylisääntöjä

--- src/datastructures/.eslintrc.js
+++ src/datastructures/.eslintrc.js
@@ -4,15 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
   },
-  plugins: ['prettier'],
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': ['error']
+    "prettier/prettier": ["error"],
   },
 };
