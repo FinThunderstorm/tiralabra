e941652ed09d7a9fe0f17a589d3dda50d6226790
Tuomas Alanen <tualanen@cs.helsinki.fi>
2021-11-11 17:43:48 +0200

    redis asetettu kuuntelemaan kontin nimeä

--- src/backend/redis.js
+++ src/backend/redis.js
@@ -1,6 +1,13 @@
 const redis = require('async-redis')
 
-const client = redis.createClient({host: "172.19.0.2"})
+const client = redis.createClient({host: "tiralabra-cache"})
 client.on('error', (error) => console.error(error))
+
+const test = async () => {
+    const status = await client.ping()
+    await console.log(`Is redis cache online? ${status}`)
+}
+
+test()
 
 module.exports = client
