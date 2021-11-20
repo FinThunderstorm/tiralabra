7fd8a808c110995081235fa7b26976b3d8a6fb36
Tuomas Alanen <tuomas@paaryna.fi>
2021-11-20 13:47:23 +0200

    lisätty HSL:n OpenTripPlanner kontti, jota vasten pyynnöt tehdään - vältetään isojen kyselymassojen tekemistä HSL:n tuotanto-API:a vasten

--- src/backend/graphql.js
+++ src/backend/graphql.js
@@ -1,7 +1,8 @@
 const { GraphQLClient } = require('graphql-request')
 
 const gQLClient = new GraphQLClient(
-    'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+    // 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+    'http://tiralabra-otp-hsl:8080/otp/routers/hsl/index/graphql'
 )
 
 module.exports = gQLClient
